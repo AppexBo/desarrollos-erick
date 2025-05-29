33dbfd17b2944b2faa5656251317b0c1e1c1e6ea
Erick Denis <erickmercadooudalova@gmail.com>
2025-05-29 20:39:00 +0300

    hide information pos

---
+++ hide_info_in_pdv_i/static/src/js/pos_information_hide.js
@@ -0,0 +1,44 @@
+/** @odoo-module */
+
+import { Order } from "@point_of_sale/app/store/models";
+import { patch } from "@web/core/utils/patch";
+
+patch(Order.prototype, {
+    setup() {
+        super.setup(...arguments);
+		this.changePos(this.pos);
+	}, 
+
+    changePos(pos){
+        const observer = new MutationObserver(() => {            
+            this.hide_in_information_finanzas();
+            this.hide_in_information_ordenars();
+        });
+        
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    },
+
+    hide_in_information_finanzas(){
+        const section_financials = document.querySelectorAll(".section-financials.mt-3.mb-4.pb-4.border-bottom.text-start");
+        section_financials.forEach(section_financial => {
+            const have_styles = section_financial.hasAttribute('style');
+            if (!have_styles) {
+                section_financial.setAttribute('style', 'display: none !important;')
+            }
+        });
+    },
+
+    hide_in_information_ordenars(){
+        const section_orders = document.querySelectorAll(".section-order.mt-3.mb-4.pb-4.border-bottom.text-start");
+        section_orders.forEach(section_order => {
+            const have_styles = section_order.hasAttribute('style');
+            if (!have_styles) {
+                section_order.setAttribute('style', 'display: none !important;')
+            }
+        });
+    },
+
+});
