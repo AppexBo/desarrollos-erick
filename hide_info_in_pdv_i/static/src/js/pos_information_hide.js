00fabbd203b60efd80b712ba9503786e55fb2151
Erick Denis <erickmercadooudalova@gmail.com>
2025-05-29 21:10:53 +0300

    hide information pos

--- hide_info_in_pdv_i/static/src/js/pos_information_hide.js
+++ hide_info_in_pdv_i/static/src/js/pos_information_hide.js
@@ -12,27 +12,16 @@ patch(Order.prototype, {
 
     changePos(pos){
         const observer = new MutationObserver(() => {
-            this.hide_info_button_products_pdv();
-            this.hide_in_information_finanzas();
-            this.hide_in_information_ordenars();
+            debugger
+            debugger
+            console.log("wtf");
+            //this.hide_in_information_finanzas();
+            //this.hide_in_information_ordenars();
         });
         
         observer.observe(document.body, {
             childList: true,
             subtree: true
-        });
-    },
-
-    hide_info_button_products_pdv(){
-        // Seleccionar todos los elementos con la clase "product-information-tag"
-        const info_buttons = document.querySelectorAll(".product-information-tag");
-        // Ocultar los elementos encontrados
-        info_buttons.forEach(info_button => {
-            // Verificar tiene el campo esta visible
-            const have_styles = info_button.hasAttribute('style');
-            if (!have_styles) {
-                info_button.setAttribute('style', 'display: none !important;')
-            }
         });
     },
 
